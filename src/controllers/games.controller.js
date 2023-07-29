22d2b5a434e4f9026f6e6c002eb960a7958d0830
MariCMelo <marilia.correa.melo@gmail.com>
2023-07-29 00:07:21 -0300

    games routes ok but not tested yet

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -6,31 +6,33 @@ export async function getGames(req, res) {
     const games = await db.query(`SELECT * FROM games;`);
     res.send(games.rows);
   } catch (err) {
-    res.status(500).send(err.message);
+    res.status(500).json({ error: "Ocorreu um erro ao buscar os jogos." });
   }
 }
 
-// export async function getGamesById(req, res) {
-//     const {id} = req.params
-//     try{
-//         const result = await db.query(`
-//         SELECT
-//             JOIN
-//             ON
-//             WHERE 
-//         `)
-//     } catch (err) {
-//       res.status(500).send(err.message)  
-//     }
-// }
-
-//POST
 export async function addGame(req, res) {
   try {
-    const game = await db.query(`INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES();`)
+    const { name, image, stockTotal, pricePerDay } = req.body;
 
-    res.send(201)
+    if (!name || !name.trim() || stockTotal <= 0 || pricePerDay <= 0) {
+      return res.status(400).send("Campos inválidos.");
+    }
+
+    const gameExists = await db.query(
+      "SELECT name FROM games WHERE name = $1",
+      [name]
+    );
+    if (gameExists.rows.length > 0) {
+      return res.status(409).send("Jogo já existente.");
+    }
+
+    const result = await db.query(
+      "INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES ($1, $2, $3, $4)",
+      [name, image, stockTotal, pricePerDay]
+    );
+
+    res.sendStatus(201);
   } catch (err) {
-    res.status(500).send(err.message)
+    res.status(500).send(err.message);
   }
 }
