e2d3fc8fafc77ab97f624bc5c8f353cf637e0b80
MariCMelo <marilia.correa.melo@gmail.com>
2023-07-30 01:20:35 -0300

    get and post rental wrote

--- src/controllers/rental.controller.js
+++ src/controllers/rental.controller.js
@@ -1,10 +1,50 @@
-import { db } from "../database/database.connection.js"
+import { db } from "../database/database.connection.js";
+import Joi from "joi";
 
 //GET
 export async function getRentals(req, res) {
   try {
-    const rentals = await db.query(`SELECT * FROM rentals;`);
-    res.send(rentals.rows);
+    const rentals = await db.query(`
+      SELECT 
+        rentals.id,
+        rentals."customerId",
+        rentals."gameId",
+        to_char(rentals."rentDate", 'YYYY-MM-DD') AS "rentDate",
+        rentals."daysRented",
+        to_char(rentals."returnDate", 'YYYY-MM-DD') AS "returnDate",
+        rentals."originalPrice",
+        rentals."delayFee",
+        customers.id AS "customer_id",
+        customers.name AS "customer_name",
+        games.id AS "game_id",
+        games.name AS "game_name"
+      FROM rentals
+      JOIN customers ON customers.id = rentals."customerId"
+      JOIN games ON games.id = rentals."gameId";
+    `);
+
+    const formattedRentals = rentals.rows.map((rental) => {
+      return {
+        id: rental.id,
+        customerId: rental.customerId,
+        gameId: rental.gameId,
+        rentDate: rental.rentDate,
+        daysRented: rental.daysRented,
+        returnDate: rental.returnDate,
+        originalPrice: rental.originalPrice,
+        delayFee: rental.delayFee,
+        customer: {
+          id: rental.customer_id,
+          name: rental.customer_name,
+        },
+        game: {
+          id: rental.game_id,
+          name: rental.game_name,
+        },
+      };
+    });
+
+    res.send(formattedRentals);
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -12,12 +52,74 @@ export async function getRentals(req, res) {
 
 //POST RENT
 export async function addRent(req, res) {
-  try {
-    const rent = await db.query(`
-      INSERT INTO rental (name, image, stockTotal, pricePerDay) 
-      VALUES();`);
+  const schema = Joi.object({
+    customerId: Joi.number().integer().positive().required(),
+    gameId: Joi.number().integer().positive().required(),
+    daysRented: Joi.number().integer().positive().min(1).required(),
+  });
 
-    res.send(201);
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
+  const { customerId, gameId, daysRented } = req.body;
+
+  try {
+    const customer = await db.query(
+      `
+      SELECT *
+      FROM customers
+      WHERE id = $1;
+      `,
+      [customerId]
+    );
+
+    if (customer.rows.length === 0) {
+      return res.status(400).json({ error: "Cliente não encontrado." });
+    }
+
+    const game = await db.query(
+      `
+      SELECT *
+      FROM games
+      WHERE id = $1;
+      `,
+      [gameId]
+    );
+
+    if (game.rows.length === 0) {
+      return res.status(400).json({ error: "Jogo não encontrado." });
+    }
+
+    const availableGames = await db.query(
+      `
+      SELECT COUNT(*) AS total_rentals
+      FROM rentals
+      WHERE "gameId" = $1 AND "returnDate" IS NULL;
+      `,
+      [gameId]
+    );
+
+    const totalRentals = availableGames.rows[0].total_rentals;
+    const { quantityInStock } = game.rows[0];
+
+    if (totalRentals >= quantityInStock) {
+      return res.status(400).json({ error: "Jogo indisponível para aluguel." });
+    }
+
+    const { pricePerDay } = game.rows[0];
+    const originalPrice = daysRented * pricePerDay;
+
+    const rent = await db.query(
+      `
+      INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "originalPrice", "returnDate", "delayFee") 
+      VALUES ($1, $2, NOW(), $3, $4, NULL, NULL);
+      `,
+      [customerId, gameId, daysRented, originalPrice]
+    );
+
+    res.sendStatus(201);
   } catch (err) {
     res.status(500).send(err.message);
   }
