7266f602136a438e5bab440678cb38d7adb3314d
MariCMelo <marilia.correa.melo@gmail.com>
2023-07-31 09:00:56 -0300

    delete route fixed add 404 validation

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 app.listen(port, () => {
 	console.log(`Servidor rodando na porta ${port}`)
 })
